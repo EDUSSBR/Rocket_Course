d0fa6ef5dbbd53cdeba03fe7b21b4997f17f2b43
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-09 09:47:24 -0300

    add decorators for injecting repo in ListCategories Usecase

--- src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
+++ src/modules/cars/useCases/listCategories/ListCategoriesUseCase.ts
@@ -1,8 +1,13 @@
 import { Category } from '../../entities/Category'
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+import { inject, injectable } from 'tsyringe'
 
+@injectable()
 export class ListCategoriesUseCase {
-  constructor (private readonly categoriesRepository: ICategoriesRepository) {}
+  constructor (
+    @inject('CategoriesRepository')
+    private readonly categoriesRepository: ICategoriesRepository) {}
+
   async execute (): Promise<Category[]> {
     const categories = await this.categoriesRepository.list()
     return categories
