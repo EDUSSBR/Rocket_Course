51556d9ff745ee29cc52d6e5ad2e12706ad51278
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-09 10:22:54 -0300

    feat: inject ImportUseCase into ImportController

--- src/modules/cars/useCases/importCategory/importCategoryController.ts
+++ src/modules/cars/useCases/importCategory/importCategoryController.ts
@@ -1,10 +1,12 @@
 import { ImportCategoryUseCase } from './importCategoryUseCase'
 import Response from 'express'
+import { container } from 'tsyringe'
 export class ImportCategoryController {
   constructor (private readonly importCategoryUseCase: ImportCategoryUseCase) {}
   async handle (req: Express.Multer.File, resp: Response): Promise<Response> {
     const { file } = req
-    await this.importCategoryUseCase.execute(file)
+    const importCategoryUseCase = container.resolve(ImportCategoryUseCase)
+    await importCategoryUseCase.execute(file)
     return resp.status(200).send()
   }
 }
