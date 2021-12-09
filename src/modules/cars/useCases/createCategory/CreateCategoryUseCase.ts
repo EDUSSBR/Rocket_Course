30fb5d5370fd56eed2729b56ef1640ee0d28ac44
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-09 09:33:58 -0300

    refactor: inject CategoryRepositorty in CreateCategoryUseCase  and turn it injectable

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.ts
@@ -1,12 +1,16 @@
 import { ICategoriesRepository } from '../../repositories/ICategoriesRepository'
+import { inject, injectable } from 'tsyringe'
 
 interface IRequest {
   name: string
   description: string
 }
 
+@injectable()
 export class CreateCategoryUseCase {
-  constructor (private readonly categoriesRepository: ICategoriesRepository) {}
+  constructor (
+    @inject('CategoryRepository')
+    private readonly categoriesRepository: ICategoriesRepository) {}
 
   async execute ({ description, name }: IRequest): Promise<void> {
     const categoryAlreadyExists = await this.categoriesRepository.findByName(name)
