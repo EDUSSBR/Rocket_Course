4b3d3e52c0e611fc899e1e0a7e53b8dca43f7aff
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-10 08:44:19 -0300

    create CreateUser migration

---
+++ src/database/migrations/1639134548796-CreateUser.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm'
+
+export class CreateUser1639134548796 implements MigrationInterface {
+  public async up (queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'users',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true
+          },
+          {
+            name: 'name',
+            type: 'varchar'
+          },
+          {
+            name: 'email',
+            type: 'varchar',
+            isUnique: true
+          },
+          {
+            name: 'password',
+            type: 'varchar'
+          },
+          {
+            name: 'driver_license',
+            type: 'varchar'
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()'
+          },
+          {
+            name: 'isAdmin',
+            type: 'boolean',
+            default: false
+          }]
+      }))
+  }
+
+  public async down (queryRunner: QueryRunner): Promise<void> {
+  }
+}
