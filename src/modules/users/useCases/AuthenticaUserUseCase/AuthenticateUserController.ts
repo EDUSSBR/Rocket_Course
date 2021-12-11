6f7cf2969a9e84144d8b32a36811d8717059b1f7
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-11 13:52:55 -0300

    add AuthenticateUserController

---
+++ src/modules/users/useCases/AuthenticaUserUseCase/AuthenticateUserController.ts
@@ -0,0 +1,15 @@
+import { AuthenticateUserUseCase } from './AuthenticateUserUseCase'
+import { container } from 'tsyringe'
+
+export class AuthenticateUserController {
+  async handle (req: Request, resp: Response): Promise<Response> {
+    try {
+      const { email, password } = req.body
+      const authenticateUserUseCase = container.resolve(AuthenticateUserUseCase)
+      const token = await authenticateUserUseCase.execute({ email, password })
+      return resp.status(200).json(token)
+    } catch (e) {
+      return resp.status(200).json(e)
+    }
+  }
+}
