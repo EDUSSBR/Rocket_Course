fd6c2541b05b8a57218db56f8d6b99c69c0a4c0b
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-11 14:00:22 -0300

    add authenticate middleware for specifications routes

--- src/routes/specifications.routes.ts
+++ src/routes/specifications.routes.ts
@@ -1,8 +1,9 @@
 import { Router } from 'express'
+import { ensureAuthenticated } from '../middleware/ensureAuthenticated'
 import { CreateSpecificationController } from '../modules/cars/useCases/createSpecification/createSpecificationController'
 
 export const specificationRoutes = Router()
 
 const createSpecificationController = new CreateSpecificationController()
-
+specificationRoutes.use(ensureAuthenticated)
 specificationRoutes.post('/', createSpecificationController.handle)
