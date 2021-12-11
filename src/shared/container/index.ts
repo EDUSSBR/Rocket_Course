3402371a33db8758b25130980ec4814bd3f164ae
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-11 09:00:53 -0300

    add singleton container for Users repository

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -3,6 +3,9 @@ import { ICategoriesRepository } from '../../modules/cars/repositories/ICategori
 import { ISpecificationRepository } from '../../modules/cars/repositories/ISpecificationRepository'
 import { CategoriesRepository } from '../../modules/cars/repositories/implementations/CategoriesRepository'
 import { SpecificationRepository } from '../../modules/cars/repositories/implementations/SpecificationRepository'
+import { IUserRepository } from '../../modules/users/repositories/IUserRepository'
+import { UserRepository } from '../../modules/users/repositories/implementations/UserRepository'
 
 container.registerSingleton<ICategoriesRepository>('CategoriesRepository', CategoriesRepository)
 container.registerSingleton<ISpecificationRepository>('SpecificationsRepository', SpecificationRepository)
+container.registerSingleton<IUserRepository>('UserRepository', UserRepository)
