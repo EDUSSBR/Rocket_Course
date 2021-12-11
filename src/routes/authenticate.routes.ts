67e73287b5f39646acb11d6ed4645251621b0093
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-11 13:58:30 -0300

    add authenticate route

---
+++ src/routes/authenticate.routes.ts
@@ -0,0 +1,9 @@
+import Router from 'express'
+import { AuthenticateUserController } from '../modules/users/useCases/AuthenticaUserUseCase/AuthenticateUserController'
+const authRoutes = Router()
+
+const authenticateUserController = new AuthenticateUserController()
+
+authRoutes.post('/sessions', authenticateUserController.handle)
+
+export { authRoutes }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,4 +1,5 @@
 import { Router } from 'express'
+import { authRoutes } from './authenticate.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
@@ -7,5 +8,5 @@ const router = Router()
 router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationRoutes)
 router.use('/users', usersRoutes)
-
+router.use(authRoutes)
 export { router }
