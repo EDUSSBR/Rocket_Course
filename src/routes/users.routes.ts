6f08ae9e3dd6b9e9af7dc7512184abf1ff1d1666
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-12 12:06:08 -0300

    add UpdateUserAvatar routeand a separated function for multer upload configs

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,9 +1,18 @@
 import Router from 'express'
+import { ensureAuthenticated } from '../middleware/ensureAuthenticated'
 import { CreateUserController } from '../modules/users/useCases/CreateUserUseCase/CreateUserController'
+import { UpdateUserAvatarController } from '../modules/users/useCases/UpdateUserAvatar/UpdateUserAvatarController'
+import multer from 'multer'
+import uploadConfig from '../config/upload'
 const usersRoutes = Router()
 
-const createUserController = new CreateUserController()
+const uploadAvatar = multer(uploadConfig.upload('./tmp/avatar'))
 
-usersRoutes.post('/', createUserController.handle )
+const createUserController = new CreateUserController()
+const updateUserAvatarController = new UpdateUserAvatarController()
+
+usersRoutes.post('/', createUserController.handle)
+
+usersRoutes.patch('/avatar', ensureAuthenticated, uploadAvatar.single('avatar'), updateUserAvatarController.handle)
 
 export { usersRoutes }
