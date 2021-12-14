121228ca306c3d2b489445b6f74c85d201259fc5
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-13 21:19:05 -0300

    create a categoryRpositoryInMemory for unit testing

---
+++ src/modules/cars/repositories/in-memory/CategoryRepositoryInMemory.ts
@@ -0,0 +1,22 @@
+import { Category } from '../../entities/Category'
+import { ICategoriesRepository, ICreateCategoryDTO } from '../ICategoriesRepository'
+
+export class CategoryRepositoryInMemory implements ICategoriesRepository {
+  categories: Category[]=[]
+
+  async findByName (name: string): Promise<Category> {
+    const category = this.categories.find((category) => category.name === name)
+    return category
+  }
+
+  async list (): Promise<Category[]> {
+    const all = this.categories
+    return all
+  }
+
+  async create ({ name, description }: ICreateCategoryDTO): Promise<void> {
+    const newCategory = new Category()
+    Object.assign(newCategory, { name, description })
+    this.categories.push(newCategory)
+  }
+}
