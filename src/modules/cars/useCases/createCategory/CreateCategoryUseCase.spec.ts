7c629d645130c1aef31a623551da0c40132bba47
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-13 21:24:12 -0300

    test: should not be able to create a new category with the same name

--- src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
+++ src/modules/cars/useCases/createCategory/CreateCategoryUseCase.spec.ts
@@ -1,5 +1,6 @@
 import { CreateCategoryUseCase } from './CreateCategoryUseCase'
 import { CategoryRepositoryInMemory } from '../../repositories/in-memory/CategoryRepositoryInMemory'
+import { AppError } from '../../../../errors/AppError'
 
 let createCategoryUseCase: CreateCategoryUseCase
 let categoryRepositoryInMemory: CategoryRepositoryInMemory
@@ -17,5 +18,15 @@ describe('Create Category', () => {
     await createCategoryUseCase.execute(category)
     const categoryCreated = await categoryRepositoryInMemory.findByName(category.name)
     expect(categoryCreated).toHaveProperty('id')
+  })
+  it('should not be able to create a new category with the same name', async () => {
+    void expect(async () => {
+      const category = {
+        name: 'New Category',
+        description: 'New category description test'
+      }
+      await createCategoryUseCase.execute(category)
+      await createCategoryUseCase.execute(category)
+    }).rejects.toBeInstanceOf(AppError)
   })
 })
