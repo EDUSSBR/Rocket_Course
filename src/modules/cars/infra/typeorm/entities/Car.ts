3908abdf4e46dff8cfce0bddcacacf05c84bac53
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-18 13:36:55 -0300

    add a new typeorm repository for cars

--- src/modules/cars/infra/typeorm/entities/Car.ts
+++ src/modules/cars/infra/typeorm/entities/Car.ts
@@ -1,25 +1,42 @@
+import { Column, Entity, JoinColumn, ManyToOne, PrimaryColumn } from 'typeorm'
 import { v4 as uuidV4 } from 'uuid'
+import { Category } from './Category'
 
+@Entity('cars')
 export class Car {
+  @PrimaryColumn()
   id: string
 
+  @Column()
   name: string
 
+  @Column()
   description: string
 
+  @Column()
   daily_rate: number
 
+  @Column()
   available: boolean
 
+  @Column()
   license_plate: string
 
+  @Column()
   fine_amount: number
 
+  @Column()
   brand: string
 
+  @Column()
   created_at: Date
 
-  category_id: string
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category
+
+  @Column()
+  category_id: string 
 
   constructor () {
     if (!this.id) {
