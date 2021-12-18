155a7e64ead8fe1965609ce497994cbfc4d721b3
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-18 13:41:05 -0300

    add cars routes for creating a Car

--- src/shared/infra/http/routes/index.ts
+++ src/shared/infra/http/routes/index.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import { authRoutes } from './authenticate.routes'
+import { carsRoutes } from './cars.routes'
 import { categoriesRoutes } from './categories.routes'
 import { specificationRoutes } from './specifications.routes'
 import { usersRoutes } from './users.routes'
@@ -8,5 +9,6 @@ const router = Router()
 router.use('/categories', categoriesRoutes)
 router.use('/specifications', specificationRoutes)
 router.use('/users', usersRoutes)
+router.use('/cars', carsRoutes)
 router.use(authRoutes)
 export { router }
