61ed5d93e5d92b5a7194ce2a7c7f7f192f04fb3b
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-19 14:36:40 -0300

    create ensureAdmin middleware

---
+++ src/shared/infra/http/middleware/ensureAdmin.ts
@@ -0,0 +1,14 @@
+import { NextFunction } from 'express'
+import { UserRepository } from '../../../../modules/users/infra/typeorm/repositories/UserRepository'
+import { AppError } from '../../../errors/AppError'
+
+export async function ensureAdmin (req: Request, resp: Response, next: NextFunction) {
+  const { id } = req.user
+  const usersRepository = new UserRepository()
+  const user = await usersRepository.findById(id)
+  if (!user.isAdmin) {
+    throw new AppError('User is not an admin')
+  }
+
+  return next()
+}
