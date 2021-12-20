d692435d2661460da3bce0ed1e7a665453506b8b
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 11:42:08 -0300

    add route for listing availables cars (all, by name, by brand, by category_id)

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,12 +1,15 @@
 import { Router } from 'express'
 import { CreateCarController } from '../../../../modules/cars/useCases/createCar/createCarController'
+import { ListAvailableCarsController } from '../../../../modules/cars/useCases/listCars/ListAvailableCarsController'
 import { ensureAdmin } from '../middleware/ensureAdmin'
 import { ensureAuthenticated } from '../middleware/ensureAuthenticated'
 
 const carsRoutes = Router()
-
+/
 let createCarController = new CreateCarController()
+const listAvailableController = new ListAvailableCarsController()
 
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle)
 
+carsRoutes.get('/available', listAvailableController.handle)
 export { carsRoutes }
