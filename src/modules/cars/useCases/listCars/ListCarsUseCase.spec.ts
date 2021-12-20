d7ee725dd014f261a0b6478bc42fcb1e5dd1b221
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 10:44:55 -0300

    test: Should be able to list all available cars by name

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -35,4 +35,17 @@ describe('List cars', () => {
     const cars = await listCarsUseCase.execute({ brand: 'CarBrand' })
     expect(cars).toEqual([car])
   })
+  it('Should be able to list all available cars by name', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car7',
+      description: 'Car0 description2',
+      daily_rate: 1004,
+      license_plate: 'abd-1235',
+      fine_amount: 65,
+      brand: 'Brand5',
+      category_id: 'category_ic5'
+    })
+    const cars = await listCarsUseCase.execute({ name: 'Car7' })
+    expect(cars).toEqual([car])
+  })
 })
