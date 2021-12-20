eab46331b7670f2b6bd72d2084aff366c2b0261d
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 10:37:56 -0300

    create ListCarsUseCase

--- src/modules/cars/useCases/listCars/ListCarsUseCase.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.ts
@@ -1,10 +1,15 @@
 import { Car } from '../../infra/typeorm/entities/Car'
 import { ICarsRepository } from '../../repositories/ICarsRepository'
 
+interface IRequest {
+  category_id?: string
+  brand?: string
+  name?: string
+}
 export class ListCarsUseCase {
   constructor (private readonly carsRepository: ICarsRepository) {}
-  async execute (): Promise<Car[]> {
-    const cars = await this.carsRepository.findAvailable()
+  async execute ({ category_id, brand, name }: IRequest): Promise<Car[]> {
+    const cars = await this.carsRepository.findAvailable(brand, category_id, name)
     return cars
   }
 }
