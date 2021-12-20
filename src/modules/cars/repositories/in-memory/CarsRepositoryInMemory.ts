dab241279114bd4a24f36a390adf5452c5b80f1c
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 10:39:29 -0300

    Create findAvailable method in CarsRepositoryInMemory and add it to the interface

--- src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
+++ src/modules/cars/repositories/in-memory/CarsRepositoryInMemory.ts
@@ -10,8 +10,21 @@ export class CarsRepositoryInMemory implements ICarsRepository {
     return car
   }
 
-  async findAvailable (): Promise<Car[]> {
-    return this.cars.filter(car => car.available)
+  async findAvailable (
+    brand?: 'string',
+    category_id?: 'string',
+    name?: 'string'
+  ): Promise<Car[]> {
+    return this.cars.filter(car => {
+      if (car.available ||
+        (brand && car.brand === brand) ||
+        (category_id && car.category_id === category_id) ||
+        (name && car.name === name)
+      ) {
+        return car
+      }
+      return null
+    })
   }
 
   async create ({
