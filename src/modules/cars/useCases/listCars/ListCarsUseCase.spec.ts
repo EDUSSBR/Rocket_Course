d699ef469b23545a4a8e8e3743743e849c002624
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 10:46:30 -0300

    test: Should be able to list all available cars by category_id

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -48,4 +48,17 @@ describe('List cars', () => {
     const cars = await listCarsUseCase.execute({ name: 'Car7' })
     expect(cars).toEqual([car])
   })
+  it('Should be able to list all available cars by category_id', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car7',
+      description: 'Car0 description2',
+      daily_rate: 1004,
+      license_plate: 'abd-1235',
+      fine_amount: 65,
+      brand: 'Brand5',
+      category_id: '123123123'
+    })
+    const cars = await listCarsUseCase.execute({ category_id: '123123123' })
+    expect(cars).toEqual([car])
+  })
 })
