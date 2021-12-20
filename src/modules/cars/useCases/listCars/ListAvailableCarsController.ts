544653e18b11c9896ca8584256ef8a9a652e1e32
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 11:41:04 -0300

    add ListAvailableCarsController

---
+++ src/modules/cars/useCases/listCars/ListAvailableCarsController.ts
@@ -0,0 +1,16 @@
+import { Response } from 'express'
+import { ListAvailableCarsUseCase } from './ListAvailableCarsUseCase'
+import { container } from 'tsyringe'
+
+export class ListAvailableCarsController {
+  async handle (req: Request, resp: Response): Promise<Response> {
+    const { brand, name, category_id } = req.query
+    const listAvailableCarsUseCase = container.resolve(ListAvailableCarsUseCase)
+    const cars = await listAvailableCarsUseCase.execute({
+      brand: brand as string,
+      name: name as string,
+      category_id: category_id as string
+    })
+    return resp.json(cars)
+  }
+}
