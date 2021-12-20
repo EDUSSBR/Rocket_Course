e85871985129b0a8ed79f4b579afe487582f451c
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2021-12-20 10:40:31 -0300

    test: Should be able to find all availables cars by brand

--- src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
+++ src/modules/cars/useCases/listCars/ListCarsUseCase.spec.ts
@@ -19,7 +19,20 @@ describe('List cars', () => {
       brand: 'Brand1',
       category_id: 'category_ic'
     })
-    const cars = await listCarsUseCase.execute()
+    const cars = await listCarsUseCase.execute(car)
+    expect(cars).toEqual([car])
+  })
+  it('Should be able to list all available cars by brand', async () => {
+    const car = await carsRepositoryInMemory.create({
+      name: 'Car0 name2',
+      description: 'Car0 description2',
+      daily_rate: 1004,
+      license_plate: 'abd-1235',
+      fine_amount: 65,
+      brand: 'Brand5',
+      category_id: 'category_ic5'
+    })
+    const cars = await listCarsUseCase.execute({ brand: 'CarBrand' })
     expect(cars).toEqual([car])
   })
 })
