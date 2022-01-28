9ea93a85bd0466ff452529021e19e5f651293bea
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-01-28 20:47:58 -0300

    fix CreateSpecificationCarController

--- src/shared/infra/http/routes/cars.routes.ts
+++ src/shared/infra/http/routes/cars.routes.ts
@@ -1,6 +1,6 @@
 import { Router } from 'express'
 import { CreateCarController } from '../../../../modules/cars/useCases/createCar/createCarController'
-import { CreateSpecificationController } from '../../../../modules/cars/useCases/createSpecification/createSpecificationController'
+import { CreateCarSpecificationController } from '../../../../modules/cars/useCases/createCarSpecification/CreateCarSpecificationController'
 import { ListAvailableCarsController } from '../../../../modules/cars/useCases/listAvailableCars/ListAvailableCarsController'
 import { ensureAdmin } from '../middleware/ensureAdmin'
 import { ensureAuthenticated } from '../middleware/ensureAuthenticated'
@@ -9,7 +9,7 @@ const carsRoutes = Router()
 
 const createCarController = new CreateCarController()
 const listAvailableController = new ListAvailableCarsController()
-const createCarSpecificationController = new CreateSpecificationController()
+const createCarSpecificationController = new CreateCarSpecificationController()
 carsRoutes.post('/', ensureAuthenticated, ensureAdmin, createCarController.handle)
 
 carsRoutes.get('/available', listAvailableController.handle)
