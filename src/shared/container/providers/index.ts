f25fee425d57eb01565f662a48f972fe76f794e9
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-01-31 11:13:52 -0300

    add DateProvider

---
+++ src/shared/container/providers/index.ts
@@ -0,0 +1,5 @@
+import { container } from 'tsyringe'
+import { IDateProvider } from './DateProviders/IDateProvider'
+import { DayjsDateProvider } from './DateProviders/implementations/DayjsDateProvider'
+
+container.registerSingleton<IDateProvider>('DayjsDateProvider', DayjsDateProvider)
