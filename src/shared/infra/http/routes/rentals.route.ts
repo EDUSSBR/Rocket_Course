79e4f67cc184c5a1d768a1f9b10691869e15bc4c
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-03-08 05:36:19 -0300

    add rentals route for devolution

--- src/shared/infra/http/routes/rentals.route.ts
+++ src/shared/infra/http/routes/rentals.route.ts
@@ -1,11 +1,14 @@
 import { Router } from 'express'
 import { CreateRentalController } from '../../../../modules/rentals/usecases/createRental/CreateRentalController'
+import { DevolutionRentalController } from '../../../../modules/rentals/usecases/DevolutionRental/DevolutionRentalController'
 import { ensureAuthenticated } from '../middleware/ensureAuthenticated'
 
 const rentalRoutes = Router()
 
 const createRentalController = new CreateRentalController()
+const devolutionRentalController = new DevolutionRentalController()
 
 rentalRoutes.post('/', ensureAuthenticated, createRentalController.handle)
+rentalRoutes.post('/devolution/:id', ensureAuthenticated, devolutionRentalController.handle)
 
 export { rentalRoutes }
