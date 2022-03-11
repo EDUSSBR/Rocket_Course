9f978be471b2201cc763eefe2e96a4dc35d9dbb5
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2022-03-11 12:59:45 -0300

    add refreshToken's route

--- src/shared/infra/http/routes/authenticate.routes.ts
+++ src/shared/infra/http/routes/authenticate.routes.ts
@@ -1,9 +1,12 @@
 import Router from 'express'
 import { AuthenticateUserController } from '../../../../modules/users/useCases/AuthenticaUserUseCase/AuthenticateUserController'
+import { RefreshTokenController } from '../../../../modules/users/useCases/refreshToken/RefreshTokenController'
 const authRoutes = Router()
 
 const authenticateUserController = new AuthenticateUserController()
+const refreshTokenController = new RefreshTokenController()
 
 authRoutes.post('/sessions', authenticateUserController.handle)
+authRoutes.post('/refresh-token', refreshTokenController.handle)
 
 export { authRoutes }
